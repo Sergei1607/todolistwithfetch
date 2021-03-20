1d9f92b131291da14f8111a595e50038b1d75abf
Sergei1607 <sergei.redondo@gmail.com>
2021-03-20 16:01:01 +0000

    Some minor fixes

--- src/js/component/list.js
+++ src/js/component/list.js
@@ -14,8 +14,31 @@ export function List() {
 	};
 
 	useEffect(() => {
+		postApi();
 		getApiData();
 	});
+
+	function postApi() {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify([]);
+
+		var requestOptions = {
+			method: "POST",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/Sergei1607",
+			requestOptions
+		)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log("error", error));
+	}
 
 	function putApiData() {
 		var myHeaders = new Headers();
